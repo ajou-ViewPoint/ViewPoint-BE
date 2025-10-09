2cd1213936cb188ca055a8d0f2da691c0e9bc8a7
jyHun <younghune135@gmail.com>
2025-10-09 17:55:46 +0900

    FEAT: add CORS config file

---
+++ src/main/java/com/www/viewpoint/global/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.www.viewpoint.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Location")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
+
+
