aacd77dbb0add694d104c261e1bf4522682b0ff1
jyHun <younghune135@gmail.com>
2025-11-17 14:51:29 +0900

    FEAT: add swagger cors

--- src/main/java/com/www/viewpoint/global/config/SwaggerConfig.java
+++ src/main/java/com/www/viewpoint/global/config/SwaggerConfig.java
@@ -6,8 +6,11 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +19,9 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("ViewPoint API").version("1.0"))
+                .servers(List.of(
+                        new Server().url("/")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components().addSecuritySchemes("bearerAuth",
                         new SecurityScheme()
